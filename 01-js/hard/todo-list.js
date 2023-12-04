7887c5827106b2f9c62a686a24694a6b53a36649
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-04 15:07:48 +0530

    fixed comments

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -14,34 +14,28 @@ class Todo {
   constructor() {
     this.todos = [];
   }
-
   add(todo) {
     this.todos.push(todo);
   }
-
   remove(indexOfTodo) {
     if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
       this.todos.splice(indexOfTodo, 1);
     }
   }
-
   update(index, updatedTodo) {
     if (index >= 0 && index < this.todos.length) {
       this.todos[index] = updatedTodo;
     }
   }
-
   getAll() {
     return this.todos;
   }
-
   get(indexOfTodo) {
     if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
       return this.todos[indexOfTodo];
     }
-    return null; // Return null if index is out of bounds
+    return null;
   }
-
   clear() {
     this.todos = [];
   }
